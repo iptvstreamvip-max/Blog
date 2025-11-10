80fe16d2d7da6599f21a570a6f1a7acbb921435e
Nawab Moazzam <cnstatus310@gmail.com>
2025-11-11 00:08:32 +0500

    Implement code changes to enhance functionality and improve performance

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,100 +1,12 @@
-import { StrapiImage } from "@/components/strapi-image";
-import { formatDate, getArticles, shimmerPlaceholder, getStrapiMedia } from "@/lib/utils";
-import Link from "next/link";
-import { Badge } from "@/components/ui/badge";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { cache } from "react";
-import {
-  Empty,
-  EmptyContent,
-  EmptyDescription,
-  EmptyMedia,
-  EmptyTitle,
-} from "@/components/ui/empty";
-import { X } from "lucide-react";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { GlobeRenderer } from "@/components/globe-renderer";
 
-const cachedArticles = cache(getArticles);
 
 export default async function Home() {
-  const article = await cachedArticles().then(
-    (articles) => articles.articles[0]
-  );
   return (
     <div className="px-8 lg:px-16 space-y-20">
       {/* Hero Section */}
-      <section className="my-10">
-        <Link
-          className="grid grid-cols-1 md:grid-cols-2 rounded-3xl group border border-transparent hover:border-border w-full hover:bg-muted overflow-hidden hover:scale-[1.02] transition duration-200 h-[calc(100vh-8rem)]"
-          href={`/blogpost/${article.slug}`}
-        >
-          <AspectRatio ratio={16 / 9} className="relative rounded-3xl bg-muted">
-            {article.cover?.url ? (
-              // <StrapiImage
-              //   src={`${article.cover.url}`}
-              //   alt={article.cover.alternativeText || "Cover Image"}
-              //   fill
-              //   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              //   className="h-full object-cover object-top w-full rounded-3xl"
-              //   placeholder={shimmerPlaceholder(
-              //     article.cover.width,
-              //     article.cover.height
-              //   )}
-              //   priority
-              // />
-              <StrapiImage
-                image={article.cover}
-                fill
-                priority={true}
-                className="rounded-3xl"
-                showCaption={true}
-              />
-            ) : (
-              <Empty className="from-muted/50 to-background h-full bg-linear-to-b from-30% rounded-3xl">
-                <EmptyContent>
-                  <EmptyMedia variant="icon">
-                    <X />
-                  </EmptyMedia>
-                  <EmptyTitle>No Image Found</EmptyTitle>
-                  <EmptyDescription>
-                    We are cooking something amazing for you.
-                  </EmptyDescription>
-                </EmptyContent>
-              </Empty>
-            )}
-          </AspectRatio>
-          <div className="p-4 md:p-8 group-hover:bg-muted flex flex-col justify-between">
-            <div>
-              <div className="flex gap-4 flex-wrap mb-4">
-                <Badge variant={"outline"} className="px-4 py-2 capitalize">
-                  {article?.category.name}
-                </Badge>
-              </div>
-              <p className="text-lg md:text-4xl font-bold mb-4">
-                {article?.title}
-              </p>
-              <p className="text-left text-base md:text-xl mt-2 text-muted-foreground">
-                {article?.description}
-              </p>
-            </div>
-            <div className="flex space-x-2 items-center  mt-6">
-              <Avatar>
-                <AvatarImage
-                  src={`${getStrapiMedia(article.author.avatar.url)}`}
-                  alt={`${article.author.avatar.alternativeText}`}
-                />
-                <AvatarFallback>{article.author.name.charAt(0)}</AvatarFallback>
-              </Avatar>
-              <p className="text-sm font-normal text-muted-foreground">
-                {article.author.name}
-              </p>
-              <div className="h-1 w-1 bg-muted-foreground rounded-full"></div>
-              <p className="text-muted-foreground text-sm  max-w-xl group-hover:text-foreground transition duration-200">
-                {formatDate(article.createdAt)}
-              </p>
-            </div>
-          </div>
-        </Link>
+      <section className="">
+        <GlobeRenderer />
       </section>
     </div>
   );
