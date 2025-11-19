e7eea4b502db39b7a88312bc837aec4858965329
Nawab Moazzam <cnstatus310@gmail.com>
2025-11-20 00:11:11 +0500

    Add smooth scrolling and improve back button functionality

--- client/components/back-button.tsx
+++ client/components/back-button.tsx
@@ -1,22 +1,21 @@
 "use client";
 import { IconArrowLeft } from "@tabler/icons-react";
-import Link from "next/link";
-import { useRouter } from "nextjs-toploader/app";
 import { Button } from "./ui/button";
 
 export default function BackButton() {
-  const router = useRouter();
   return (
     <div className="flex justify-between items-center px-2 pb-2">
       <Button
         variant={"ghost"}
         onClick={() => {
-          router.back();
+          window.history.back();
         }}
-        className="flex space-x-2 items-center"
+        className="flex space-x-2 items-center group"
       >
-        <IconArrowLeft className="w-4 h-4 text-muted-foreground" />
-        <span className="text-sm text-muted-foreground">Back</span>
+        <IconArrowLeft className="w-4 h-4 text-muted-foreground group-hover:text-primary" />
+        <span className="text-sm text-muted-foreground group-hover:text-primary">
+          Back
+        </span>
       </Button>
     </div>
   );
